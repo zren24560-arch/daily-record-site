ec965f866e139d3313b2839dc76538bdee28b0d7
zren24560-arch <zren24560@gmail.com>
2026-06-11 17:56:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // Supabase 初始化
 // ===========================
 const SUPABASE_URL = "https://yvppdssdbxqfbvnuiulb.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_Uhi-GEGSmeosY1snKFzcCQ_NUMXQITn";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl2cGRkc3NkYnhxZmJ2bnVpdWxiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2MzM0MTAsImV4cCI6MjA5NjIwOTQxMH0.zukPv4euFKoDIjttLyH4PgHcV6zuH9D9k1uQ7eKph7w";
 const { createClient } = supabase;
 const db = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
